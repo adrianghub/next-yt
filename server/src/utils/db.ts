cec3d1574e36d012c5daf0a56a8c484c598a60ab
Adrian Zinko <adrian.zinko@boldare.com>
2022-07-10 22:38:36 +0200

    feat(): config docker

--- server/src/utils/db.ts
+++ server/src/utils/db.ts
@@ -1,10 +1,12 @@
-
-import mongoose from 'mongoose';
-import logger from './logger';
+import mongoose from "mongoose";
+import logger from "./logger";
 
 export async function initializeDbConnection() {
   try {
-    await mongoose.connect(process.env.DB_CONNECTION_STRING || "");
+    await mongoose.connect(
+      `${process.env.DB_CONNECTION_STRING}:${process.env.DB_CONNECTION_PORT}` ||
+        ""
+    );
     logger.info("Sucessfully connected to database.");
   } catch (error) {
     logger.error(error, "Connection to database failed.");
